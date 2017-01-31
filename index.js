a1f983d5384e473dde2a0fca30394337207c3c86
stephan <railsc0d0r@gmail.com>
2017-01-31 05:48:55 +0100

    Lets express listen on port 4000 now.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 var express = require('express');
 var app = express();
+var port = 4000;
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Server listening on port ' + port + '!');
 });
