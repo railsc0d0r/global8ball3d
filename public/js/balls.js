362283c206ec9ecd3790c7b55e42f130fed09f53
stephan <railsc0d0r@gmail.com>
2017-01-31 05:44:52 +0100

    Adds balls-config in separat file.

---
+++ public/js/balls.js
@@ -0,0 +1,177 @@
+var BALLS = [{
+        "id": 1,
+        "type": "breakball",
+        "color": "white",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": -0.635,
+            "z": 0
+        }
+    },
+    {
+        "id": 2,
+        "type": "playball",
+        "color": "red",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.5321946429994914,
+            "z": 0.0
+        }
+    },
+    {
+        "id": 3,
+        "type": "playball",
+        "color": "gold",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.5835973214997456,
+            "z": -0.0291
+        }
+    },
+    {
+        "id": 4,
+        "type": "playball",
+        "color": "red",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.5835973214997456,
+            "z": 0.030100000000000002
+        }
+    },
+    {
+        "id": 5,
+        "type": "playball",
+        "color": "red",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.635,
+            "z": -0.0582
+        }
+    },
+    {
+        "id": 6,
+        "type": "8ball",
+        "color": "black",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.635,
+            "z": 0.0010000000000000009
+        }
+    },
+    {
+        "id": 7,
+        "type": "playball",
+        "color": "gold",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.635,
+            "z": 0.060200000000000004
+        }
+    },
+    {
+        "id": 8,
+        "type": "playball",
+        "color": "gold",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.6864026785002544,
+            "z": -0.0873
+        }
+    },
+    {
+        "id": 9,
+        "type": "playball",
+        "color": "red",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.6864026785002544,
+            "z": -0.0281
+        }
+    },
+    {
+        "id": 10,
+        "type": "playball",
+        "color": "gold",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.6864026785002544,
+            "z": 0.031100000000000003
+        }
+    },
+    {
+        "id": 11,
+        "type": "playball",
+        "color": "red",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.6864026785002544,
+            "z": 0.0903
+        }
+    },
+    {
+        "id": 12,
+        "type": "playball",
+        "color": "gold",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.7378053570005086,
+            "z": -0.1164
+        }
+    },
+    {
+        "id": 13,
+        "type": "playball",
+        "color": "red",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.7378053570005086,
+            "z": -0.0572
+        }
+    },
+    {
+        "id": 14,
+        "type": "playball",
+        "color": "gold",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.7378053570005086,
+            "z": 0.0020000000000000018
+        }
+    },
+    {
+        "id": 15,
+        "type": "playball",
+        "color": "red",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.7378053570005086,
+            "z": 0.061200000000000004
+        }
+    },
+    {
+        "id": 16,
+        "type": "playball",
+        "color": "gold",
+        "radius": 0.0291,
+        "mass": 0.17,
+        "position": {
+            "x": 0.7378053570005086,
+            "z": 0.12040000000000001
+        }
+    }
+]
