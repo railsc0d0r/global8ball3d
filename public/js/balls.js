4aa0b8ac0576f13d896b38d0384edfe5b515088e
stephan <railsc0d0r@gmail.com>
2017-02-04 06:49:42 +0100

    Refactors balls-definition to be more DRY. Removes double-quotes.

--- public/js/balls.js
+++ public/js/balls.js
@@ -1,177 +1,165 @@
+var ball_radius = 0.0291;
+var ball_mass = 0.17;
+
 var BALLS = [{
-        "id": 1,
-        "type": "breakball",
-        "color": "white",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": -0.635,
-            "z": 0
+        id: 1,
+        type: "breakball",
+        color: "white",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: -0.635,
+            z: 0
         }
-    },
-    {
-        "id": 2,
-        "type": "playball",
-        "color": "red",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.5321946429994914,
-            "z": 0.0
+    }, {
+        id: 2,
+        type: "playball",
+        color: "red",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.5321946429994914,
+            z: 0.0
         }
-    },
-    {
-        "id": 3,
-        "type": "playball",
-        "color": "yellow",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.5835973214997456,
-            "z": -0.0291
+    }, {
+        id: 3,
+        type: "playball",
+        color: "yellow",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.5835973214997456,
+            z: -0.0291
         }
-    },
-    {
-        "id": 4,
-        "type": "playball",
-        "color": "red",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.5835973214997456,
-            "z": 0.030100000000000002
+    }, {
+        id: 4,
+        type: "playball",
+        color: "red",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.5835973214997456,
+            z: 0.030100000000000002
         }
-    },
-    {
-        "id": 5,
-        "type": "playball",
-        "color": "red",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.635,
-            "z": -0.0582
+    }, {
+        id: 5,
+        type: "playball",
+        color: "red",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.635,
+            z: -0.0582
         }
-    },
-    {
-        "id": 6,
-        "type": "8ball",
-        "color": "black",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.635,
-            "z": 0.0010000000000000009
+    }, {
+        id: 6,
+        type: "8ball",
+        color: "black",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.635,
+            z: 0.0010000000000000009
         }
-    },
-    {
-        "id": 7,
-        "type": "playball",
-        "color": "yellow",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.635,
-            "z": 0.060200000000000004
+    }, {
+        id: 7,
+        type: "playball",
+        color: "yellow",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.635,
+            z: 0.060200000000000004
         }
-    },
-    {
-        "id": 8,
-        "type": "playball",
-        "color": "yellow",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.6864026785002544,
-            "z": -0.0873
+    }, {
+        id: 8,
+        type: "playball",
+        color: "yellow",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.6864026785002544,
+            z: -0.0873
         }
-    },
-    {
-        "id": 9,
-        "type": "playball",
-        "color": "red",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.6864026785002544,
-            "z": -0.0281
+    }, {
+        id: 9,
+        type: "playball",
+        color: "red",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.6864026785002544,
+            z: -0.0281
         }
-    },
-    {
-        "id": 10,
-        "type": "playball",
-        "color": "yellow",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.6864026785002544,
-            "z": 0.031100000000000003
+    }, {
+        id: 10,
+        type: "playball",
+        color: "yellow",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.6864026785002544,
+            z: 0.031100000000000003
         }
-    },
-    {
-        "id": 11,
-        "type": "playball",
-        "color": "red",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.6864026785002544,
-            "z": 0.0903
+    }, {
+        id: 11,
+        type: "playball",
+        color: "red",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.6864026785002544,
+            z: 0.0903
         }
-    },
-    {
-        "id": 12,
-        "type": "playball",
-        "color": "yellow",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.7378053570005086,
-            "z": -0.1164
+    }, {
+        id: 12,
+        type: "playball",
+        color: "yellow",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.7378053570005086,
+            z: -0.1164
         }
-    },
-    {
-        "id": 13,
-        "type": "playball",
-        "color": "red",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.7378053570005086,
-            "z": -0.0572
+    }, {
+        id: 13,
+        type: "playball",
+        color: "red",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.7378053570005086,
+            z: -0.0572
         }
-    },
-    {
-        "id": 14,
-        "type": "playball",
-        "color": "yellow",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.7378053570005086,
-            "z": 0.0020000000000000018
+    }, {
+        id: 14,
+        type: "playball",
+        color: "yellow",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.7378053570005086,
+            z: 0.0020000000000000018
         }
-    },
-    {
-        "id": 15,
-        "type": "playball",
-        "color": "red",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.7378053570005086,
-            "z": 0.061200000000000004
+    }, {
+        id: 15,
+        type: "playball",
+        color: "red",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.7378053570005086,
+            z: 0.061200000000000004
         }
-    },
-    {
-        "id": 16,
-        "type": "playball",
-        "color": "yellow",
-        "radius": 0.0291,
-        "mass": 0.17,
-        "position": {
-            "x": 0.7378053570005086,
-            "z": 0.12040000000000001
+    }, {
+        id: 16,
+        type: "playball",
+        color: "yellow",
+        radius: ball_radius,
+        mass: ball_mass,
+        position: {
+            x: 0.7378053570005086,
+            z: 0.12040000000000001
         }
     }
 ]
