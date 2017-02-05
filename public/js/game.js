f434d6cc34fb481f6d654f605d1909c39379572e
Stephan Barth <stephan.barth@niemau.de>
2017-02-05 08:49:25 +0100

    Defines brown as material from hex-string.

--- public/js/game.js
+++ public/js/game.js
@@ -63,7 +63,7 @@ function GAME(balls, borders, holes) {
         blue: BABYLON.Color3.Blue(),
         gray: BABYLON.Color3.Gray(),
         green: BABYLON.Color3.Green(),
-        brown: BABYLON.Color3.Green().add(BABYLON.Color3.Red()),
+        brown: BABYLON.Color3.FromHexString('#663300'),
         lightBlue: BABYLON.Color3.Blue().add(BABYLON.Color3.Gray())
       };
 
