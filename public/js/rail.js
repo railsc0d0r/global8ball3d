966997e49dc3fdab1b1362a7a9d943f5f2b68e6a
stephan <railsc0d0r@gmail.com>
2017-02-07 04:17:49 +0100

    Redefines the rail as boxes.

--- public/js/rail.js
+++ public/js/rail.js
@@ -5,105 +5,49 @@ var x_outer_limit = 1.4782;
 var z_inner_limit = 0.6932;
 var z_outer_limit = 0.8432;
 
-var RAIL = {
-    vertices: [
-	{
-	    x: -x_outer_limit,
-	    y: upper_limit,
-	    z: -z_outer_limit
-	},
-	{
-	    x: x_outer_limit,
-	    y: upper_limit,
-	    z: -z_outer_limit
-	},
-	{
-	    x: x_outer_limit,
-	    y: upper_limit,
-	    z: z_outer_limit
-	},
-	{
-	    x: -x_outer_limit,
-	    y: upper_limit,
-	    z: z_outer_limit
-	},
-	{
-	    x: -x_inner_limit,
-	    y: upper_limit,
-	    z: -z_inner_limit
-	},
-	{
-	    x: x_inner_limit,
-	    y: upper_limit,
-	    z: -z_inner_limit
-	},
-	{
-	    x: x_inner_limit,
-	    y: upper_limit,
-	    z: z_inner_limit
-	},
-	{
-	    x: -x_inner_limit,
-	    y: upper_limit,
-	    z: z_inner_limit
-	},
-	{
-	    x: -x_outer_limit,
-	    y: lower_limit,
-	    z: -z_outer_limit
-	},
-	{
-	    x: x_outer_limit,
-	    y: lower_limit,
-	    z: -z_outer_limit
-	},
-	{
-	    x: x_outer_limit,
-	    y: lower_limit,
-	    z: z_outer_limit
-	},
-	{
-	    x: -x_outer_limit,
-	    y: lower_limit,
-	    z: z_outer_limit
-	},
-	{
-	    x: -x_inner_limit,
-	    y: lower_limit,
-	    z: -z_inner_limit
-	},
-	{
-	    x: x_inner_limit,
-	    y: lower_limit,
-	    z: -z_inner_limit
-	},
-	{
-	    x: x_inner_limit,
-	    y: lower_limit,
-	    z: z_inner_limit
-	},
-	{
-	    x: -x_inner_limit,
-	    y: lower_limit,
-	    z: z_inner_limit
-	}
-    ],
-    faces: [
-	[0,1,5,4],
-	[1,2,6,5],
-	[2,3,7,6],
-	[3,0,4,7],
-	[8,9,13,12],
-	[9,10,14,13],
-	[10,11,15,14],
-	[11,8,12,15],
-	[0,8,9,1],
-	[1,9,10,2],
-	[2,10,11,3],
-	[3,11,8,0],
-	[4,12,13,5],
-	[5,13,14,6],
-	[6,14,15,7],
-	[4,12,15,7]
-    ]
-};
+var RAIL_BOXES = [
+    {
+      id: "left",
+      width: x_outer_limit - x_inner_limit,
+      height: upper_limit - lower_limit,
+      depth: 2 * z_outer_limit,
+      position: {
+        x: x_outer_limit - (x_outer_limit - x_inner_limit) / 2,
+        y: 0,
+        z: 0
+      }
+    },
+    {
+      id: "right",
+      width: x_outer_limit - x_inner_limit,
+      height: upper_limit - lower_limit,
+      depth: 2 * z_outer_limit,
+      position: {
+        x: -(x_outer_limit - (x_outer_limit - x_inner_limit) / 2),
+        y: 0,
+        z: 0
+      }
+    },
+    {
+      id: "top",
+      width: 2 * x_outer_limit,
+      height: upper_limit - lower_limit,
+      depth: z_outer_limit - z_inner_limit,
+      position: {
+        x: 0,
+        y: 0,
+        z: z_outer_limit - (z_outer_limit - z_inner_limit) / 2
+      }
+    },
+    {
+      id: "bottom",
+      width: 2 * x_outer_limit,
+      height: upper_limit - lower_limit,
+      depth: z_outer_limit - z_inner_limit,
+      position: {
+        x: 0,
+        y: 0,
+        z: -(z_outer_limit - (z_outer_limit - z_inner_limit) / 2)
+      }
+    }
+];
