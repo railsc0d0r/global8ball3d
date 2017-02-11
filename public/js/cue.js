4fb6371ca9f9f988c5b7a2a9c2c842f2a572a33d
stephan <railsc0d0r@gmail.com>
2017-02-11 05:05:28 +0100

    Adds definition for cue-stick.

---
+++ public/js/cue.js
@@ -0,0 +1,62 @@
+var CUE = [
+    {
+      name: 'tip',
+      diameterTop: 0.014,
+      diameterBottom: 0.014,
+      height: 0.005,
+      color: 'black',
+      position: {
+        x: 0,
+        y: 0.03,
+        z: 0
+      }
+    },
+    {
+      name: 'ferule',
+      diameterTop: 0.014,
+      diameterBottom: 0.014,
+      height: 0.045,
+      color: 'white',
+      position: {
+        x: 0,
+        y: 0.035,
+        z: 0
+      }
+    },
+    {
+      name: 'taper',
+      diameterTop: 0.014,
+      diameterBottom: 0.014,
+      height: 0.3,
+      color: 'lightBrown',
+      position: {
+        x: 0,
+        y: 0.08,
+        z: 0
+      }
+    },
+    {
+      name: 'shaft',
+      diameterTop: 0.035,
+      diameterBottom: 0.014,
+      height: 0.6,
+      color: 'lightBrown',
+      position: {
+        x: 0,
+        y: 0.38,
+        z: 0
+      }
+    },
+    {
+      name: 'butt',
+      diameterTop: 0.035,
+      diameterBottom: 0.035,
+      height: 0.6,
+      color: 'brown',
+      position: {
+        x: 0,
+        y: 0.98,
+        z: 0
+      }
+    }
+];
